24c14ab4f748e5f24a51ccf83bfc44786dc7572b
Dawid Karp <dadulerk@gmail.com>
2023-09-24 17:52:51 +0200

    feat: add more typehints

--- ising.py
+++ ising.py
@@ -1,5 +1,4 @@
 import sys
-from typing import Any
 
 import networkx as nx
 import numpy as np
@@ -12,35 +11,35 @@ np.random.seed(2001)
 @jit(nopython=True, cache=True)
 def simulate(
     *,
-    steps: float,
-    spins: npt.NDArray[Any],
-    edges: npt.NDArray[Any],
+    steps: int,
+    spins: npt.NDArray[float64],
+    edges: npt.NDArray[float64],
     temp: float,
 ):
     np.random.seed(2001)
 
     N = spins.shape[0]
 
-    init_energy: float = -np.sum(edges @ spins * spins) / 2.0
-    init_magnet: float = np.sum(spins)
+    init_energy: float64 = -1.0 * np.sum(edges @ spins * spins) / 2.0
+    init_magnet: float64 = np.sum(spins)
 
-    energy = init_energy
-    magnet = init_magnet
+    energy: float64 = init_energy
+    magnet: float64 = init_magnet
 
     for step in range(steps):
-        idx = np.random.randint(N)
+        idx: int = np.random.randint(N)
 
-        spins[idx] *= -1  # change spin value
-        new_energy = -np.sum(edges @ spins * spins) / 2.0
+        spins[idx] *= -1.0  # change spin value
+        new_energy: float64 = -1.0 * np.sum(edges @ spins * spins) / 2.0
 
-        dE = new_energy - energy
-        dM = -2 * spins[idx]
+        dE: float64 = new_energy - energy
+        dM: float64 = -2.0 * spins[idx]
 
         if dE < 0.0 or (np.random.random() < min(np.exp(-dE / temp), 1.0)):
             energy += dE
             magnet += dM
         else:
-            spins[idx] *= -1  # restore prev spin value, reject change
+            spins[idx] *= -1.0  # restore prev spin value, reject change
 
         print(temp, step, energy, magnet)
 
@@ -55,10 +54,10 @@ def generate(*, n: int, k: int, edges: npt.NDArray[float64]):
         layers: npt.NDArray[float64] = np.empty((k, *edges.shape))
 
         for ki in range(k):
-            layers[ki] = np.linalg.matrix_power(edges, ki + 1)
+            layers[ki]: npt.NDArray[float64] = np.linalg.matrix_power(edges, ki + 1)
 
-        edges = (layers != 0).argmax(axis=0)
-        edges = np.where(edges != 0, np.exp(-edges), 0)
+        edges: npt.NDArray[float64] = (layers != 0).argmax(axis=0)
+        edges: npt.NDArray[float64] = np.where(edges != 0, np.exp(-edges), 0)
 
     return spins, edges
 
