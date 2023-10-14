b361848ad216a5896342ba2ffe1f0f0c2492a329
davkk <dadulerk@gmail.com>
2023-10-14 23:45:00 +0200

    refactor: J matrix generation update (aka weight calculation)
    for now, it will involve calculation of a weight for a given interation based on how many paths there are between two spins and how long these paths are

--- ising/graph.py
+++ ising/graph.py
@@ -2,32 +2,29 @@ import argparse
 
 import numpy as np
 from networkx import barabasi_albert_graph, to_numpy_array
-from numba import jit
-
-np.random.seed(2001)
 
 
-# @jit(nopython=True, fastmath=True, cache=True, parallel=True)
 def generate(*, edges: np.ndarray, r: np.uint8):
-    edges = edges.astype(np.float32)
+    if r == 1:
+        return edges
+
     n = edges.shape[0]
+    layers = np.empty((r, n, n))
 
-    if r > 1:
-        layers = np.empty((r, n, n), dtype=np.float32)
+    for k in range(r):
+        layers[k] = np.linalg.matrix_power(edges, k + 1)
+        layers[k] *= np.exp(-(k + 1))
 
-        for k in range(r):
-            layers[k] = np.linalg.matrix_power(edges, k + 1)
-            layers[k] = (layers[k] > 0.0) * (k + 1)
+    layers = np.sum(layers, axis=0)
+    layers = layers / np.max(layers)
 
-        layers = np.argmax(layers, 0) + 1.0
-        edges = np.exp(-layers).astype(np.float32)
-
-    return edges
+    return layers
 
 
+# %%
 def main():
     parser = argparse.ArgumentParser(
-        description="Tool to generate barabasi graph"
+        description="Tool to generate a Barabasi-Albert graph"
     )
     parser.add_argument(
         "-n",
@@ -44,27 +41,29 @@ def main():
         help="barabasi parameter",
     )
     parser.add_argument(
-        "-k",
+        "-r",
         metavar="int",
         type=np.uint8,
         required=True,
         help="range of interactions",
     )
-    n, m, k = parser.parse_args().__dict__.values()
+    n, m, r = parser.parse_args().__dict__.values()
+
+    np.random.seed(2001)
 
     edges = generate(
         edges=to_numpy_array(
             barabasi_albert_graph(n=n, m=m),
-            dtype=np.float32,
+            dtype=np.float64,
             order="C",
         ),
-        r=k,
+        r=r,
     )
 
-    print(edges)
+    print(f"{edges=}")
 
     np.save(
-        f"barabasi_{n=}_{m=}_{k=}",
+        f"barabasi_{n=}_{m=}_{r=}",
         edges,
     )
 
