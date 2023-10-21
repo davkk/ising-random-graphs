10a3a141b7e53e9e000cdcd46d023ece22a46607
davkk <dadulerk@gmail.com>
2023-10-21 12:55:13 +0200

    feat: add ER graph generation

--- ising/graph.py
+++ ising/graph.py
@@ -1,18 +1,34 @@
 import argparse
+from enum import Enum
 
+import networkx as nx
 import numpy as np
-from networkx import barabasi_albert_graph, to_numpy_array
 
 
-def generate(*, edges: np.ndarray, r: np.uint8):
+def short_path(*, graph: nx.Graph):
+    n = graph.number_of_nodes()
+    edges = np.empty((n, n), dtype=np.float64, order="C")
+
+    for node in graph.nodes():
+        layers = enumerate(nx.bfs_layers(graph, node))
+
+        for length, conn in layers:
+            edges[node, conn] = np.exp(-length)
+
+        edges[node, node] = 0
+
+    return edges
+
+
+def mat_power(*, graph: np.ndarray, r: np.uint8):
     if r == 1:
-        return edges
+        return graph
 
-    n = edges.shape[0]
+    n = graph.shape[0]
     layers = np.empty((r, n, n))
 
     for k in range(r):
-        layers[k] = np.linalg.matrix_power(edges, k + 1)
+        layers[k] = np.linalg.matrix_power(graph, k + 1)
         layers[k] *= np.exp(-(k + 1))
 
     layers = np.sum(layers, axis=0)
@@ -21,10 +37,33 @@ def generate(*, edges: np.ndarray, r: np.uint8):
     return layers
 
 
+class Method(Enum):
+    shortpath = "shortpath"
+    matpower = "matpower"
+
+
+class Graph(Enum):
+    ER = "ER"
+    BA = "BA"
+
+
 # %%
 def main():
     parser = argparse.ArgumentParser(
-        description="Tool to generate a Barabasi-Albert graph"
+        description="Tool to generate a random graph"
+    )
+    parser.add_argument(
+        "graph",
+        type=str,
+        choices=Graph._member_names_,
+        help="graph type",
+    )
+    parser.add_argument(
+        "--method",
+        type=str,
+        choices=Method._member_names_,
+        help="method of generating the graph",
+        default=Method.shortpath,
     )
     parser.add_argument(
         "-n",
@@ -34,36 +73,58 @@ def main():
         help="number of nodes",
     )
     parser.add_argument(
+        "-p",
+        metavar="int",
+        type=np.float64,
+        help="probability for edge creation",
+    )
+    parser.add_argument(
         "-m",
         metavar="int",
         type=np.uint8,
-        required=True,
-        help="barabasi parameter",
+        help="number of edges to attach",
     )
-    parser.add_argument(
-        "-r",
-        metavar="int",
-        type=np.uint8,
-        required=True,
-        help="range of interactions",
-    )
-    n, m, r = parser.parse_args().__dict__.values()
+    graph_type, method, n, p, m = parser.parse_args().__dict__.values()
+
+    graph, edges, r, filename_params = None, None, None, None
 
     np.random.seed(2001)
 
-    edges = generate(
-        edges=to_numpy_array(
-            barabasi_albert_graph(n=n, m=m),
-            dtype=np.float64,
-            order="C",
-        ),
-        r=r,
-    )
+    match graph_type:
+        case Graph.ER.value:
+            if not p:
+                parser.error("The -p flag is required Erdos-Renyi graph")
+
+            graph = nx.erdos_renyi_graph(n=n, p=p)
+            r = np.ceil(np.emath.logn((n - 1) * p, n))
+            filename_params = f"p={int(p * 100)}"
+
+        case Graph.BA.value:
+            if not m:
+                parser.error("The -m flag is required Barabasi-Albert graph")
+
+            graph = nx.barabasi_albert_graph(n=n, m=m)
+            r = np.ceil(np.emath.logn(2 * m, n))
+            filename_params = f"{m=}"
+
+    match method:
+        case Method.matpower.value:
+            edges = mat_power(
+                graph=nx.to_numpy_array(
+                    graph,
+                    dtype=np.float64,
+                    order="C",
+                ),
+                r=r,
+            )
+
+        case Method.shortpath.value:
+            edges = short_path(graph=graph)
 
     print(f"{edges=}")
 
     np.save(
-        f"barabasi_{n=}_{m=}_{r=}",
+        f"data/graphs/{graph_type}_{method}_{n=}_" + filename_params,
         edges,
     )
 
