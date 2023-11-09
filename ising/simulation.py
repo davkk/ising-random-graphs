90f6d3aed159b24f0ddba28e9fa5584c1eb7bc2a
davkk <dadulerk@gmail.com>
2023-11-09 15:20:28 +0100

    fix: compute X correctly

--- ising/simulation.py
+++ ising/simulation.py
@@ -5,7 +5,7 @@ from numba.pycc import CC
 cc = CC("ising")
 
 
-@nb.njit(parallel=True, fastmath=True, cache=True)
+@nb.njit(parallel=True, cache=True)
 @cc.export("simulate", "Array(f8, 2, 'C'), i8, f8")
 def simulate(graph, steps, temp):
     n = graph.shape[0]
@@ -30,47 +30,7 @@ def simulate(graph, steps, temp):
             magnet += dM
 
         if step % n == 0:
-            print(temp, step // n, energy / n, magnet / n)
-
-
-@nb.njit(parallel=True, fastmath=True, cache=True)
-@cc.export("shortpath", "Array(f8, 2, 'C')(Array(u1, 2, 'C'))")
-def shortpath(graph):
-    n = graph.shape[0]
-
-    J = np.zeros(n * n, dtype=np.float64)
-
-    q = np.full(n, -1, dtype=np.uint8)
-
-    for node in range(n):
-        path_len = np.zeros(n, np.uint16)
-        path_len[node] = 0
-
-        front, rear = 0, 1
-        q[front] = node
-
-        while front < rear:
-            curr = q[front]
-            front += 1
-
-            J[node * n + curr] = path_len[curr]
-
-            for child in range(n):
-                if graph[curr, child] == 0:
-                    continue
-
-                if path_len[child] > 0:
-                    continue
-
-                path_len[child] = path_len[curr] + 1
-
-                q[rear] = child
-                rear += 1
-
-    J = J.reshape(n, n)
-    J = np.exp(-(J - 1))
-    np.fill_diagonal(J, 0)
-    return J
+            print(temp, step // n, energy / n, abs(magnet) / n)
 
 
 def main():
